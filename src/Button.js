699ac0213ac2b2886346cc8f163a43333bf65a9f
nachumfreedman <nochum09@gmail.com>
2018-09-03 00:47:17 +0300

    addeda  button and img of spaceman

---
+++ src/Button.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import './Button.css';
+
+class Button extends Component {
+
+  render() {
+    return (
+      <div>
+        
+        <div class="basicBox">
+          Hire us
+          <svg width="130" height="65" viewBox="0 0 130 65" xmlns="http://www.w3.org/2000/svg">
+            <rect x='0' y='0' fill='none' width='130' height='65'/>
+          </svg>
+        </div>
+
+        
+      </div>
+    );
+  }
+}
+
+export default Button;
--- src/Home.js
+++ src/Home.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
+import './Home.css';
+import Button from './Button';
+import Spaceman from './imgs/spaceman.png';
 
 
 class Home extends Component {
 
   render() {
     return (
-      <div className="App">
-        <h1>Your next interactive experince</h1>
-        <button>Hire us</button>
+      <div className="home-page">
+        <h1 className='home-page-header'>Your next interactive experince</h1>
+        <Button></Button>
+        <img src={Spaceman} alt='Welcome'/>
+        <div className='-home-page-border-line'/>
       </div>
     );
   }
