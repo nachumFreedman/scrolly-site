7885f8e3849e77cff8248f97d066aa7a0baf6f61
nachumfreedman <nochum09@gmail.com>
2018-09-02 20:17:33 +0300

    first commit with the scroll function ready

---
+++ src/App.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  
+  state = { lastPosition: 0,
+            pageNumber: 0,
+            blockScroll: false,
+  }
+
+  componentDidMount() {
+    window.addEventListener('scroll', this.handleScroll);
+  };
+
+  componentWillUnmount() {
+    window.removeEventListener('scroll', this.handleScroll);
+  };
+
+  handleScroll = (event) => {
+    const currentPosition = window.scrollY;
+    const distance = currentPosition - this.state.lastPosition;
+    const direction = distance > 0 ? 1 : -1;
+    
+    if (!this.state.blockScroll) {
+      Math.abs(distance) > 10 ? this.setState({
+        pageNumber: this.state.pageNumber + direction,
+        blockScroll: true,
+      }, () => {
+        setTimeout(()=>{this.setState({blockScroll: false})}, 1500)
+      }
+      ) : null
+    }
+    
+    this.setState({
+      lastPosition: currentPosition,
+    });
+  };
+
+  
+  render() {
+    console.log(this.state.pageNumber)
+    return (
+      <div className="App">
+        
+      </div>
+    );
+  }
+}
+
+export default App;
